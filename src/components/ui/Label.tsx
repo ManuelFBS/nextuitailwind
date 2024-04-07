a3a88486fa945ef9c6c6dfc8148ebebcc8913a0b
ManuelFBS <manuelf.borrego@gmail.com>
2024-04-07 08:45:22 -0400

    Actualización de page.tsx, Input.tsx; Creación de Label.tsx y Button.tsx

---
+++ src/components/ui/Label.tsx
@@ -0,0 +1,16 @@
+interface Props
+  extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+
+export default function Label({
+  children,
+  ...props
+}: Props) {
+  return (
+    <label
+      className="block text-sm font-bold leading-6 text-gray-700"
+      {...props}
+    >
+      {children}
+    </label>
+  );
+}
